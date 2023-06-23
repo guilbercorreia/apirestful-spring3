38829cebf5347ad56c38ca33409b3face2972b9c
Guilber Correia <guilber.correia.gc@gmail.com>
2023-06-23 20:23:17 -0300

    Obter usuário por ID

--- src/main/java/com/apirestful/demo/services/UserService.java
+++ src/main/java/com/apirestful/demo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.apirestful.demo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.apirestful.demo.domain.User;
 import com.apirestful.demo.repositories.UserRepository;
+import com.apirestful.demo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
