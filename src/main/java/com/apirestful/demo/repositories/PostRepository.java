1a5ff2f56a5c58a6d29fceeadf4113166da71ecd
Guilber Correia <guilber.correia.gc@gmail.com>
2023-06-29 18:40:19 -0300

    Métodos de busca simples e com critérios

--- src/main/java/com/apirestful/demo/repositories/PostRepository.java
+++ src/main/java/com/apirestful/demo/repositories/PostRepository.java
@@ -3,7 +3,15 @@ package com.apirestful.demo.repositories;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.apirestful.demo.domain.Post;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.Date;
+import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String>{
 
+    List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ $and: [ { 'date': { $gte: ?1 } }, { date: { $lte: ?2 } } , { $or: [ { 'title': { $regex: ?0 , $options: 'i' } }, { 'body': { $regex: ?0 , $options: 'i' } }, { 'comments.text': { $regex: ?0 , $options: 'i' } } ] } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
--- src/main/java/com/apirestful/demo/resources/PostResource.java
+++ src/main/java/com/apirestful/demo/resources/PostResource.java
@@ -1,14 +1,15 @@
 package com.apirestful.demo.resources;
 
+import com.apirestful.demo.resources.util.URL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.apirestful.demo.domain.Post;
 import com.apirestful.demo.services.PostService;
+
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -21,5 +22,23 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok(obj);
+	}
+
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok(list);
+	}
+
+	@GetMapping(value = "/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch (@RequestParam(value = "text", defaultValue = "") String text ,
+												  @RequestParam(value = "minDate", defaultValue = "") String minDate,
+												  @RequestParam(value = "maxDate", defaultValue = "") String maxDate){
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok(list);
 	}
 }
